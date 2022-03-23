f5dbd813dd83f1857866dbc0ce3e14103ff1413d
Yurino Oga <yurino.o@latona.io>
2022-03-23 09:45:25 +0900

    release

---
+++ SAP_API_Caller/requests/to_call_objects.go
@@ -0,0 +1,13 @@
+package requests
+
+type ToCallObjects struct {
+			MaintenancePlan              string      `json:"MaintenancePlan"`
+			MaintenancePlanCallNumber    int         `json:"MaintenancePlanCallNumber"`
+			MaintenanceItem              string      `json:"MaintenanceItem"`
+			MaintenanceOrder             *string      `json:"MaintenanceOrder"`
+			MaintenanceNotification      *string      `json:"MaintenanceNotification"`
+			ServiceOrder                 *string      `json:"ServiceOrder"`
+			MaintCallHorizonIsNotReached *bool        `json:"MaintCallHorizonIsNotReached"`
+			SchedulingStatus             *string      `json:"SchedulingStatus"`
+			PlannedStartDate             *string      `json:"PlannedStartDate"`
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-maintenance-item-creates/SAP_API_Caller"
+	sap_api_input_reader "sap-api-integrations-maintenance-item-creates/SAP_API_Input_Reader"
+	"sap-api-integrations-maintenance-item-creates/config"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+	sap_api_post_header_setup "github.com/latonaio/sap-api-post-header-setup"
+	sap_api_time_value_converter "github.com/latonaio/sap-api-time-value-converter"
+)
+
+func main() {
+	l := logger.NewLogger()
+	conf := config.NewConf()
+	fr := sap_api_input_reader.NewFileReader()
+	pc := sap_api_post_header_setup.NewSAPPostClientWithOption(conf.SAP)
+	caller := sap_api_caller.NewSAPAPICaller(
+		conf.SAP.BaseURL(),
+		"100",
+		pc,
+		l,
+	)
+	inputSDC := fr.ReadSDC("./Inputs/SDC_Maintenance_Item_sample.json")
+	sap_api_time_value_converter.ChangeTimeFormatToSAPFormatStruct(&inputSDC)
+
+	accepter := getAccepter(inputSDC)
+	item := inputSDC.ConvertToItem()
+
+	caller.AsyncPostMaintenanceItem(
+		item,
+		accepter,
+	)
+}
+
+func getAccepter(sdc sap_api_input_reader.SDC) []string {
+	accepter := sdc.Accepter
+	if len(sdc.Accepter) == 0 {
+		accepter = []string{"All"}
+	}
+
+	if accepter[0] == "All" {
+		accepter = []string{
+			"Item",
+		}
+	}
+	return accepter
+}
